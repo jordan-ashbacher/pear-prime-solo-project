418b7618b0d4c67fff88e1bcc6bcd902e8c0bbd6
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-02-08 17:57:51 -0600

    updates database sql file

--- database.sql
+++ database.sql
@@ -13,3 +13,23 @@ CREATE TABLE "user" (
     "password" VARCHAR (1000) NOT NULL,
     "city" VARCHAR (80) NOT NULL
 );
+
+CREATE TABLE "restaurant" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) not null,
+	"image" varchar(1000) not null,
+	"place_id" varchar(100) not null
+);
+
+CREATE TABLE "favorite" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" int references "user",
+	"restaurant_id" int references "restaurant",
+	"notes" text
+);
+
+CREATE TABLE "friend" (
+	"id" SERIAL PRIMARY KEY,
+	"user1_id" int references "user",
+	"user2_id" int references "user"
+);
