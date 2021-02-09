eba1f36a31c88bfd82c65346fee0b316071eee5f
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-02-08 21:58:59 -0600

    moves favorite to restauarant router, deletes detail

--- server/routes/restaurant.router.js
+++ server/routes/restaurant.router.js
@@ -1,6 +1,8 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+require("dotenv").config();
 const {
     rejectUnauthenticated,
   } = require("../modules/authentication-middleware.js");
@@ -8,45 +10,65 @@ const {
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
-});
-
-/**
- * POST route template
- */
-router.post('/', rejectUnauthenticated, (req, res) => {
-  // POST route code here
-  console.log('restaurant post user:', req.user)
-  console.log(req.body)
-
-  const restaurantQuery = `INSERT INTO restaurant (name, image, place_id)
-                VALUES ($1, $2, $3)
-                RETURNING id`
-
-    pool
-    .query(restaurantQuery, [req.body.name, req.body.image, req.body.place_id])
-    .then(result => {
-        console.log('new restaurant id:', result.rows[0].id)
-
-        const restaurantID = result.rows[0].id
-
-        const favoriteQuery = `INSERT INTO favorite (user_id, restaurant_id)
-                VALUES ($1, $2)`
-
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+    // GET route code here
+  
+    const id = req.params.id
+    axios
+    .get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&key=${process.env.GOOGLE_API_KEY}`)
+    .then(results => {
+        const restaurant = {
+            city: results.data.result.address_components[3].long_name,
+            address: results.data.result.formatted_address,
+            phone: results.data.result.formatted_phone_number,
+            name: results.data.result.name,
+            photo1: results.data.result.photos[0].photo_reference,
+            photo2: results.data.result.photos[1].photo_reference,
+            photo3: results.data.result.photos[2].photo_reference,
+            photo4: results.data.result.photos[3].photo_reference,
+            photo5: results.data.result.photos[4].photo_reference,
+            place_id: results.data.result.place_id,
+            rating: results.data.result.rating,
+            website: results.data.result.website
+        }
+  
+        console.log(restaurant)
+  
+        const restaurantQuery = `INSERT INTO restaurant (city, address, phone, name, photo1, photo2, photo3, photo4, photo5, place_id, rating, website)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+        RETURNING id`
+  
         pool
-        .query(favoriteQuery, [req.user.id, restaurantID])
+        .query(restaurantQuery, [restaurant.city, restaurant.address, restaurant.phone, restaurant.name, restaurant.photo1, restaurant.photo2, restaurant.photo3, restaurant.photo4, restaurant.photo5, restaurant.place_id, restaurant.rating, restaurant.website])
         .then(result => {
-            res.sendStatus(201)
-        }).catch(err => {
-            console.log(err)
-            res.sendStatus(500)
-        })
+          console.log('new restaurant id:', result.rows[0].id)
+  
+          const restaurantID = result.rows[0].id
+  
+          const favoriteQuery = `INSERT INTO favorite (user_id, restaurant_id)
+                  VALUES ($1, $2)`
+  
+          pool
+          .query(favoriteQuery, [req.user.id, restaurantID])
+          .then(result => {
+              res.sendStatus(201)
+          }).catch(err => {
+              console.log(err)
+              res.sendStatus(500)
+          })
+      })
+      .catch(err => {
+          console.log(err)
+          res.sendStatus(500)
+      })
+  
+  
     })
     .catch(err => {
         console.log(err)
-        res.sendStatus(500)
-    })
-});
+    }) 
+  
+  
+  });
 
 module.exports = router;
--- src/redux/sagas/restaurant.saga.js
+++ src/redux/sagas/restaurant.saga.js
@@ -4,7 +4,7 @@ import { put, takeEvery } from 'redux-saga/effects'
 function* addRestaurant(action) {
     try {
         const restaurant = action.payload
-        yield axios.get(`/api/detail/${restaurant.place_id}`)
+        yield axios.get(`/api/restaurant/${restaurant.place_id}`)
         // yield axios.post('/api/restaurant', restaurant)
     } catch(err) {
         console.log('Error in addRestaurant saga', err)
