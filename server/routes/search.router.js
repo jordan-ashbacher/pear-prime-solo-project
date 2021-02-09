9a103600874a9793d6afba8f51e2115ebcd96aef
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-02-09 14:21:50 -0600

    updates search api path to use users lat and lng

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -16,7 +16,7 @@ router.get('/:q', rejectUnauthenticated, (req, res) => {
   const query = req.params.q
   console.log(query)
   axios
-  .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=45.032490,-93.267290&radius=32000&type=restaurant&keyword=${query}&key=${process.env.GOOGLE_API_KEY}`)
+  .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.user.lat},${req.user.lng}&radius=32000&type=restaurant&keyword=${query}&key=${process.env.GOOGLE_API_KEY}`)
   .then((response) => {
       const results = response.data.results.map((place) => ({
           place_id: place.place_id,
