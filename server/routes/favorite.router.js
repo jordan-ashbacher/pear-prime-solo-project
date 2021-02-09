7ff5fa6ec2f99daff29a7f9d27d0ca5a2ef31c5b
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-02-08 19:00:09 -0600

    writes favorite post route with query

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+    rejectUnauthenticated,
+  } = require("../modules/authentication-middleware.js");
 
 /**
  * GET route template
@@ -12,8 +15,17 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
   // POST route code here
+  console.log('favorite post user:', req.user)
+  const restaurantID = req.body.place_id
+  console.log(req.body)
+
+  const query = `INSERT INTO favorite (user_id, restaurant_id)
+                VALUES ($1, $2)`
+
+  
+
 });
 
 module.exports = router;
