9f2fcd2e8ca0589903bd3359b7a9e7a4d5c8051c
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-02-16 15:09:59 -0600

    fixes match query bug

--- server/routes/pair.router.js
+++ server/routes/pair.router.js
@@ -10,6 +10,7 @@ const {
 router.get('/:friendID', rejectUnauthenticated, (req, res) => {
     const friendID = req.params.friendID
     const userID = req.user.id
+    const userCity = req.user.current_location
     const query = `SELECT A.restaurant_id, restaurant.address, restaurant.city, restaurant.name, restaurant.phone, restaurant.photo1, restaurant.photo2, restaurant.photo3, restaurant.photo4, restaurant.photo5, restaurant.place_id, restaurant.rating, restaurant.id, restaurant.website, COUNT(A.restaurant_id) FROM favorite A
     INNER JOIN (SELECT restaurant_id, COUNT(*)
                 FROM favorite
@@ -18,12 +19,12 @@ router.get('/:friendID', rejectUnauthenticated, (req, res) => {
             ) B ON A.restaurant_id = B.restaurant_id
     JOIN restaurant ON restaurant.id = A.restaurant_id
     JOIN "user" ON "user".id = A.user_id		
-    WHERE user_id = $1 OR user_id = $2 AND "user".current_location = restaurant.city
+    WHERE user_id = $1 OR user_id = $2 AND restaurant.city = $3
     GROUP BY A.restaurant_id, restaurant.address, restaurant.city, restaurant.name, restaurant.phone, restaurant.photo1, restaurant.photo2, restaurant.photo3, restaurant.photo4, restaurant.photo5, restaurant.place_id, restaurant.rating, restaurant.id, restaurant.website
     HAVING COUNT(*) > 1;`
 
     pool
-    .query(query, [userID, friendID])
+    .query(query, [userID, friendID, userCity])
     .then(results => {
         console.log(results.rows)
         res.send(results.rows)
